5af16db8de5d9fc0df7d3b24a8b9bce44c047b14
Sam Chittenden <sbchittenden@gmail.com>
2017-06-29 14:11:06 -0600

    Updated exercise file

--- mastering-jquery/exercises/mjq-ex2.js
+++ mastering-jquery/exercises/mjq-ex2.js
@@ -8,3 +8,31 @@ let newP = $('<p>');
 newP.text('I\'m new here');
 newP.appendTo(someNew);
 $('body').append(someNew);
+$('.something-new > p').remove();
+$('.something-new').addClass('people-list');
+
+
+//Load exercises/data/mjq.json from AJAX and create individual paragraphs inside the people listfor each person. The text should be their name, and the class should be their tag.
+
+$.ajax('data/mjq.json', {
+  success: function(data, status, jqXHR) {
+    data.forEach(obj => {
+      let pg = $('<p>').addClass(obj.tag).text(obj.name);
+      $('.people-list').append(pg);
+    });
+    //Now query for the collection of paragraphs within the .something-new div and limit to only the first and third elements, then add the zebra class to those items.
+    let pgraph = $('.something-new');
+    pgraph.find('p').filter(':even').addClass('zebra');
+    //Finally, loop through the whole collection and prepend their tag within a span with the class role.
+    let role = $('<span>').addClass('role');
+    pgraph.find('p').before(role);
+  },
+  error: function(jqXHR, status) {
+    // status is a string description of why request failed
+    console.warn("Failed with status:", status);
+  },
+  complete: function(jqXHR, status) {
+    // Do any cleanup needed regardless of status
+    console.log('call complete');
+  }
+});
